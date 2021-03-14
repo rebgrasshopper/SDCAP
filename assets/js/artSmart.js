a51fe1e845ddbbab3a1eebf9c8718ac7e44470da
Plover Brown <56779568+rebgrasshopper@users.noreply.github.com>
2021-03-13 20:14:12 -0800

    artSmart page

---
+++ assets/js/artSmart.js
@@ -0,0 +1,17 @@
+function expander(event) {
+    if (event.target.classList.contains("title")) {
+        const descriptionDiv = event.target.parentNode.childNodes[3];
+        const carat = event.target.childNodes[1]
+        if (descriptionDiv.classList.contains("closed")) {
+            descriptionDiv.classList.remove("closed")
+            carat.innerHTML = "▴"
+        } else {
+            descriptionDiv.classList.add("closed")
+            carat.innerHTML = "▾"
+        }
+
+    }
+
+}
+
+document.addEventListener("click", expander);
