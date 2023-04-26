ce76b6603a6121b5dfc2b3f238fa49b04ea710f5
Plover <rellwoos@gmail.com>
2023-04-25 23:38:13 -0700

    added location details for banker's hill

--- assets/js/classes.js
+++ assets/js/classes.js
@@ -94,76 +94,76 @@ const DESCRIPTIONS = {
   [SUBJECTS.THEATRE]: {
     description:
       "Participation in a drama class has been proven for gains in reading proficiency, improvement in self-concept, motivation, and higher levels of empathy and tolerance towards others among youth. Drama activities help develop social and language skills, and help students to open up and express themselves with their peers. It challenges them to quickly and creatively come up with solutions to the moment. The skills developed through theater not only train you how to convincingly deliver a message, but they also build the confidence you need to take command of the stage.",
-    bankersHillDetails: "",
-    chulaDetails: "",
+    BankersHillDetails: "",
+    ChulaVistaDetails: "",
   },
   [SUBJECTS.SINGING]: {
     description:
       "Students will learn to read music, vocalize, and properly use their diaphragm & breathe, while learning about Music Theory. Students will also learn to sing songs from Broadway shows as well as pop music.",
-    bankersHillDetails: "",
-    chulaDetails: "",
+    BankersHillDetails: "",
+    ChulaVistaDetails: "",
   },
   [SUBJECTS.VISUAL_ARTS]: {
     description:
       " Through Color Mixing, Painting, Drawing, and Sculpting with “no fire” clay students will explore different art forms. Students will use Math to aid in sculpting, drawing shapes that might be found in their neighborhood, and Science observations for painting clouds, weather, etc. In 'Life Maps and Books,' students will use mixed media to create a collage highlighting high points or important events in their life.",
-    bankersHillDetails: "",
-    chulaDetails: "",
+    BankersHillDetails: "",
+    ChulaVistaDetails: "",
   },
   [SUBJECTS.DANCE]: {
     description:
       "This Dance class includes the techniques of Jazz, Hip Hop, Contemporary, and Musical Theatre basics. Students will learn how to count and dance rhythms, travel across the floor, form patterns, and shapes with their bodies, all while getting fit, motivated, and having fun with their peers.",
-    bankersHillDetails: "",
-    chulaDetails: "",
+    BankersHillDetails: "",
+    ChulaVistaDetails: "",
   },
   [SUBJECTS.BALLET_TAP_1]: {
     description:
       "Ballet & Tap Combination Class for children 6 - 7 years of age. Students learn the basics and vocabulary of Ballet and Tap, increasing their flexibility, strength, coordination, and rhythm.",
-    bankersHillDetails: "",
-    chulaDetails: "",
+    BankersHillDetails: "",
+    ChulaVistaDetails: "",
   },
   [SUBJECTS.BALLET_TAP_2]: {
     description:
       "Ballet & Tap Combination Class for children 8 - 10 years of age. Students will advance, building on the same skills in Kids 1 Ballet & Tap, and learning new dance techniques and vocabulary.",
-    bankersHillDetails: "",
-    chulaDetails: "",
+    BankersHillDetails: "",
+    ChulaVistaDetails: "",
   },
   [SUBJECTS.KIDS_JAZZ_1_2]: {
     description:
       "ZOOM ONLINE class\n\nJazz online class suitable for mixed ages. Students will learn and advance dance techniques.",
-    bankersHillDetails: "",
-    chulaDetails: "",
+    BankersHillDetails: "",
+    ChulaVistaDetails: "",
   },
   [SUBJECTS.BALLET_TAP_0]: {
     description:
       "This class is an introduction to dance for the youngest students. They will learn how to be in a class, how to move together, and gain the fundamentals of creative Ballet & Tap.",
-    bankersHillDetails: "",
-    chulaDetails: "",
+    BankersHillDetails: "",
+    ChulaVistaDetails: "",
   },
   [SUBJECTS.TUMBLING]: {
     description:
       "A beginner and intermediate tumbling moves class. Students will improve strength and flexibility while learning how to move safely.",
-    bankersHillDetails: "",
-    chulaDetails: "",
+    BankersHillDetails: "",
+    ChulaVistaDetails: "",
   },
   [SUBJECTS.ADULT_SENIOR_DANCE]: {
     description:
       "A Dance Movement class for Adults & Seniors to work on  Balance, Grace, to Strengthen and Improve Flexibility incorporates elements of Modern Dance, Pilates, Yoga, a little Musical Theatre and “All That Jazz” Students will explore their creativity through Music, Dance, and Theatre.  Dress in comfortable clothing and shoes. Tennis shoes or non-slip shoes with padded soles are recommended.",
-    bankersHillDetails: "",
-    chulaDetails:
+    BankersHillDetails: "",
+    ChulaVistaDetails:
       "$40 or pay what you can\nThursdays 2:30-3:15  April 12 - May 31 at Parkway Community Center",
   },
   [SUBJECTS.PRE_DANCE_TUMBLE]: {
     description:
       "In this age appropriate intro to dance, music, and tumbling, students will explore their creativity through imaginative lessons, all while moving and training their bodies in Creative Ballet, Tap, and Tumbling. Colorful ideas and imagery are incorporated into the curriculum to engage young minds and bodies.  Students should wear any color ballet leotard (skirt ok), and footless or transitional Ballet Pink tights, Pink Ballet slippers, Black Tap shoes, Bare feet for Tumbling and Tumbling mat. (Ikea has some reasonably priced mats)",
-    bankersHillDetails: "",
-    chulaDetails:
+    BankersHillDetails: "",
+    ChulaVistaDetails:
       "$40\nWednesdays 2:30-3:15   April 13 - June 1 at CV Woman's Club (no class May 11)",
   },
   [SUBJECTS.MUSICAL_THEATRE_KIDS]: {
     description:
       "Students will study Acting, Singing, and Dance while rehearsing for the amazing fun production. This performance opportunity provides youth to engage socially, collaborate and team build while learning stage directions, making connections, and memories.\nSDCAP Director Laura Hodge has written (“rebooted”) an updated version of Alice in Wonderland with music from the 80’s, 90’s, and 2000’s. There’s a part for everyone in this magical exciting production. All students will be cast. All roles are open to everyone.\nStudents should always dress “Ready to Move”- comfortable clothing and shoes. Please bring a notebook, Binder, Pencils,Highlighter, and water bottle.",
-    bankersHillDetails: "",
-    chulaDetails:
+    BankersHillDetails: "",
+    ChulaVistaDetails:
       '"Alice in Wonderland" Rebooted!\n$50\nTwo 1 ½ hour Classes per week\nWednesdays 3:30-5:00 April 12 - May 31 at Parkway Community Center\nThursdays 3:30-5:00  April 13 - June 1   at CV Woman’s Club (no class May 11)',
   },
 };
@@ -226,11 +226,10 @@ $(document).ready(function () {
             }"></a>
             <h4><strong>${className}</strong></h4>
             ${
-              DESCRIPTIONS[className].chulaDetails
-                ? `<strong>${DESCRIPTIONS[className].chulaDetails.replace(
-                    /\n/g,
-                    "<br/>"
-                  )}</strong><br/><br/>`
+              DESCRIPTIONS[className][location + "Details"]
+                ? `<strong>${DESCRIPTIONS[className][
+                    location + "Details"
+                  ].replace(/\n/g, "<br/>")}</strong><br/><br/>`
                 : ""
             }
             ${DESCRIPTIONS[className].description}`;
